d743a3ceff07e7c6fbf9a64a960519ede68ef74a
R3ality <idle.relay@gmail.com>
2018-12-01 18:03:52 +0200

    Switch from Array to Hashtable keys

--- 01/part2.ps1
+++ 01/part2.ps1
@@ -1,10 +1,28 @@
 $In = Get-Content "input.txt"
 $Res = $Index = 0
+$Hasht = @{}
+while ($true) {
+    If ($Index -gt ($In.Count-1)) { $Index = 0 }
+    $Res += $In[$Index]
+    If ($Hasht.ContainsKey($Res)) { Write-Host "First recurring frequency is" $Res; Break }
+    $Hasht.add($Res, $true)
+    $Index++
+}
+
+<#
+# Original approach below was using an Array. This was crazy slow.
+# In case of $Arr -contains $Res the solution took around 1163,449 seconds.
+# $Arr.Contains($Res) was much faster at 752,417 seconds, but obviously still not close to being reasonable
+# Hashtable key approach above took around 1,2 seconds for the solution
+
+$In = Get-Content "input.txt"
+$Res = $Index = 0
 $Arr = @()
 while ($true) {
     If ($Index -gt ($In.Count-1)) { $Index = 0 }
     $Res += $In[$Index]
-    If ($Arr -contains $Res) { Write-Host "First recurring frequency is" $Res; Break }
+    If ($Arr.Contains($Res)) { Write-Host "First recurring frequency is" $Res; Break }
     $Arr += $Res
     $Index++
 }
+#>
