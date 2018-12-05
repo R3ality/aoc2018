5f997a8e908e9413cd63d4d41806d7f31dd08eda
R3ality <idle.relay@gmail.com>
2018-12-05 21:07:40 +0200

    Minor perf fix

--- 05/part2.js
+++ 05/part2.js
@@ -11,9 +11,9 @@ let lengths = [];
 let alphabet = 'abcdefghijklmnopqrstuvwxyz';
 for (let j = 0; j < alphabet.length; j++) { // loop alphabet
     let copy = JSON.parse(JSON.stringify(input));
-    copy = Array.from(copy.replace(new RegExp('[' + alphabet[j] + alphabet[j].toUpperCase() +  ']', 'g'), '')); // remove units of this type
+    copy = copy.replace(new RegExp('[' + alphabet[j] + alphabet[j].toUpperCase() +  ']', 'g'), ''); // remove units of this type
     
-    copy = copy.reduce((accum, current) => {
+    copy = copy.split('').reduce((accum, current) => {
         if (accum.length === 0) accum.push(current); // first char
         else {
             let prev = accum[accum.length-1];
